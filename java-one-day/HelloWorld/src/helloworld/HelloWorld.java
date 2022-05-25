8c14f4d061991922c280323e9d30f2bc2573fc8c
Lauren Morgan <rg326@live.com>
2022-05-24 20:58:47 -0400

    attempt at fixing class error

--- java-one-day/HelloWorld/src/helloworld/HelloWorld.java
+++ java-one-day/HelloWorld/src/helloworld/HelloWorld.java
@@ -2,7 +2,9 @@ package helloworld;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        //Prints 'Hello World' to screen
+        //Prints 'Hello World'
         System.out.println("Hello World");
     }
 }
+
+//Error log: find out how to get main class recognized by Netbeans
